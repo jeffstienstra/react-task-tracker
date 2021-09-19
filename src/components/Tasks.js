5398de03492d52778788b174b2c4dd1d97abea05
jeffstienstra <jeffstienstra@gmail.com>
2021-09-18 21:31:40 -0400

    edit tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,14 @@
-import Task from './Task'
+import Task from "./Task";
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-  
   return (
-  <>
-  {tasks.map((task) => (
-    <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
-  ))}
-  </>
-  )
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+      <p>Double click a task to toggle reminder.</p>
+    </>
+  );
 };
 
 export default Tasks;
